6809e0aa113bd0fddac50c013a888c21fd827f38
Jørgen Bergquist <gismos@gmail.com>
2014-02-23 13:53:22 +0100

    wip: add replies and comment to forum post

--- client/js/forum.js
+++ client/js/forum.js
@@ -9,4 +9,31 @@ module.exports.threadView = function(){
 
     console.log('forum', forum);
     console.log('post', forum.get('post'));
+
+    forum.on('addReply', function(event){
+        var reply = {
+            mdtext: 'ny reply #' + new Date().getTime()
+        };
+        console.log('event', event);
+        forum.addReply(reply);
+    });
+
+    forum.on('addComment', function(event){
+        var comment = {
+            mdtext: 'super kommentar #'+ new Date().getTime()
+        };
+
+        console.log('comment event', event);
+        forum.addComment(event.context._id, comment);
+    });
+
+    forum.on('removeReply', function(event){
+        forum.deleteReply({_id: 'newID'});
+    });
+
+    forum.on('removeComment', function(event){
+        forum.deleteComment('newID', {_id: 'newCommentID'});
+    });
+
+    return forum;
 };
