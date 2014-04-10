3a64f8396e0a9d397450be66a56f1546e1502f9c
Sigurd Gartmann <sigurdga@sigurdga.no>
2014-04-10 11:27:37 +0200

    List activities on frontpage

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,32 @@
 var crypto = require('crypto'),
-    User = require('../models').User;
+    User = require('../models').User,
+    Activity = require('../models').Activity;
 
 // core routes - base is /
 module.exports.index = function(req, res) {
-    res.render('index');
+    var query = Activity.find().or([
+        {'permissions.public': true},
+        {'permissions.users': req.user._id},
+        {'permissions.groups': { $in: req.user.groups }}
+    ])
+    .sort('-modified')
+    .limit(20);
+    if (req.query.page) {
+        query = query.skip(20 * req.query.page);
+    }
+    query.populate('users', 'profile_picture_path').exec(function (err, activities) {
+        if (err) {
+            throw err;
+        }
+        res.format({
+            html: function () {
+                res.render('index', {activities: activities});
+            },
+            json: function () {
+                res.json(200, activities);
+            }
+        });
+    });
 };
 
 module.exports.login = function(req, res){
