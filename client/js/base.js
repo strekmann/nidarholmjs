3e1e22f2cb2b84440e30025bce202ee31e85a849
Sigurd Gartmann <sigurdga@sigurdga.no>
2014-04-13 18:45:04 +0200

    Ractify activity list and make it close to pretty

--- client/js/base.js
+++ client/js/base.js
@@ -1,8 +1,19 @@
 module.exports = {
-    indexView: function(){
+    indexView: function(activities){
         var ractive = new Ractive({
             el: '#ractive',
             template: '#tmpl-test'
+        });
+
+        var ractive_activities = new Ractive({
+            el: '#activities',
+            template: '#activity-template',
+            data: {
+                activities: activities,
+                ago: function (date) {
+                    return moment(date).from();
+                }
+            }
         });
     }
 };
