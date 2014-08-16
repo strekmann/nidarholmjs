9b78029891e9afb99d0536e60f3d65ca408ebf17
Sigurd Gartmann <sigurdga@sigurdga.no>
2014-08-16 13:29:43 +0200

    Check that members should be put in list, also on mailing lists

--- server/routes/organization.js
+++ server/routes/organization.js
@@ -710,7 +710,7 @@ module.exports.encrypted_mailman_lists = function (req, res, next) {
                         }
                         else {
                             var emails = _.reduce(g.members, function (list, member) {
-                                if (member.user.email && _.find(member.user.groups, function (group) {
+                                if (member.user.email && member.user.in_list && _.find(member.user.groups, function (group) {
                                     return group === req.organization.member_group._id;
                                 })) {
                                     list.push(member.user.email);
