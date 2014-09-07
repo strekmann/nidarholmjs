99c1781e5728ed33ede088c9f64df079a2d9da9d
Sigurd Gartmann <sigurdga@sigurdga.no>
2014-09-07 23:26:22 +0200

    startOf should be better for separating upcoming and finished projects

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -30,7 +30,7 @@ module.exports.index = function (req, res, next) {
     else {
         query = Project.find().or({'permissions.public': true});
     }
-    query.where({end: {$gt: moment().endOf('day')}});
+    query.where({end: {$gt: moment().startOf('day')}});
     query.sort('end')
         .populate('creator', 'username name');
     query.exec(function (err, projects) {
@@ -51,7 +51,7 @@ module.exports.index = function (req, res, next) {
                 else {
                     query = Project.find({'permissions.public': true});
                 }
-                query.where({end: {$lt: moment().endOf('day')}});
+                query.where({end: {$lt: moment().startOf('day')}});
                 query.sort('-end')
                     .populate('creator', 'username name');
 
