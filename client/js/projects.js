31d581e1d2048b5516c5a76ce48ab5b1188c329b
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-08-25 01:40:03 +0200

    Make events and date ranges look the same as frontpage

--- client/js/projects.js
+++ client/js/projects.js
@@ -130,35 +130,35 @@ var Project = Ractive.extend({
                 if (startm.isSame(endm, 'day')) {
                     // same day
                     if (startm.isSame(startd) && endm.isSame(endd)) {
-                        return '<time class="start" datetime="' + startm.format() + '">' + startm.format('LL') + '</time>';
+                        return '<time class="start" datetime="' + startm.format() + '">' + startm.format('ll') + '</time>';
                     }
-                    return '<time class="start" datetime="' + startm.format() + '">' + startm.format('LLL') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('LT') + '</time>';
+                    return '<time class="start" datetime="' + startm.format() + '">' + startm.format('lll') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('LT') + '</time>';
                 }
                 //console.log(startm, startd, startm.isSame(startd));
                 //console.log(endm, endd, endm.isSame(endd));
                 // saving dates should always set startOf('day') AND later wholeday
                 if (startm.isSame(startd) && endm.isSame(endd)) {
-                    return '<time class="start" datetime="' + startm.format() + '">' + startm.format('LL') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('LL') + '</time>';
+                    return '<time class="start" datetime="' + startm.format() + '">' + startm.format('ll') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('ll') + '</time>';
                 }
-                return '<time class="start" datetime="' + startm.format() + '">' + startm.format('LLL') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('LLL') + '</time>';
+                return '<time class="start" datetime="' + startm.format() + '">' + startm.format('lll') + '</time> – <time class="end" datetime="' + endm.format() + '">' + endm.format('lll') + '</time>';
             }
             if (start) {
                 // only start
                 startm = moment(start);
                 startd = moment(startm).startOf('day');
                 if (startm.isSame(startd, 'second')) {
-                    return '<time datetime="' + startm.format() + '">' + startm.format('LL') + '</time>';
+                    return '<time datetime="' + startm.format() + '">' + startm.format('ll') + '</time>';
                 }
-                return '<time datetime="' + startm.format() + '">' + startm.format('LLL') + '</time>';
+                return '<time datetime="' + startm.format() + '">' + startm.format('lll') + '</time>';
             }
             if (end) {
                 // only end
                 endm = moment(end);
                 endd = moment(endm).startOf('day');
                 if (endm.isSame(endd, 'second')) {
-                    return '<time datetime="' + endm.format() + '">' + endm.format('LL') + '</time>';
+                    return '<time datetime="' + endm.format() + '">' + endm.format('ll') + '</time>';
                 }
-                return '<time datetime="' + endm.format() + '">' + endm.format('LLL') + '</time>';
+                return '<time datetime="' + endm.format() + '">' + endm.format('lll') + '</time>';
             }
         },
         permission_options: function () {
