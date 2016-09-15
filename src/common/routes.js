3951ef26d50256d70ee9e6d0565f7177aa99552e
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-09-16 01:48:17 +0200

    Add simple projects page

--- src/common/routes.js
+++ src/common/routes.js
@@ -4,6 +4,8 @@ import { Route, IndexRoute, createRoutes } from 'react-router';
 
 import App from './containers/App';
 import Home from './containers/Home';
+import Projects from './containers/Projects';
+
 import About from './components/About';
 
 export const queries = {
@@ -11,9 +13,14 @@ export const queries = {
     organization: () => Relay.QL`query { organization }`,
 };
 
+export const organizationQueries = {
+    organization: () => Relay.QL`query { organization }`,
+};
+
 export default createRoutes(
     <Route path="/" component={App} queries={queries}>
         <IndexRoute component={Home} queries={queries} />
+        <Route path="projects" component={Projects} queries={organizationQueries} />
         <Route path="about" component={About} />
     </Route>
 );
