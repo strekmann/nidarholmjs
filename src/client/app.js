dc75bb8f5be739035478877b5f009bff4e731606
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-09-22 17:48:40 +0200

    More webpack, more norsk

--- src/client/app.js
+++ src/client/app.js
@@ -6,8 +6,11 @@ import IsomorphicRelay from 'isomorphic-relay';
 import IsomorphicRouter from 'isomorphic-relay-router';
 import { match, Router, browserHistory } from 'react-router';
 import routes from '../common/routes';
-import moment from 'moment';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import moment from 'moment';
+import 'moment/locale/nb';
+import 'font-awesome/css/font-awesome.css';
+import './scss/styles.scss';
 
 injectTapEventPlugin();
 
@@ -16,8 +19,6 @@ environment.injectNetworkLayer(new Relay.DefaultNetworkLayer('/graphql', {
     credentials: 'same-origin',
 }));
 IsomorphicRelay.injectPreparedData(environment, window.__INITIAL_STATE__);
-
-moment.locale('nb');
 
 match({ routes, history: browserHistory }, (error, redirectLocation, renderProps) => {
     IsomorphicRouter.prepareInitialRender(environment, renderProps).then(props => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import path from 'path';
 
 module.exports = {
     entry: './src/client/app.js',
@@ -27,6 +28,29 @@ module.exports = {
                     presets: 'react-hmre',
                 },
             },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'postcss', 'sass?outputStyle=expanded'],
+            },
+            {
+                test: /fontawesome-webfont\.(eot|woff|woff2|ttf|svg)/,
+                loader: 'file?name=src/client/fonts/[name].[ext]',
+            },
+            {
+                test: /\.(png|jpg|svg|eot|woff|woff2|ttf)$/,
+                loader: 'file',
+            },
         ],
     },
+    sassLoader: {
+        includePaths: [
+            path.resolve(__dirname, 'node_modules/bootstrap-sass/assets/stylesheets'),
+            path.resolve(__dirname, 'node_modules/font-awesome/scss'),
+            path.resolve(__dirname, 'node_modules/foundation-sites/scss'),
+        ]
+    }
 };
