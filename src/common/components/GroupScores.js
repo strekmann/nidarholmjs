e67448da46b8ce311bd8f319a0aabc2c91e6c493
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-10-10 02:40:13 +0200

    Designing upload page for music

--- src/common/components/GroupScores.js
+++ src/common/components/GroupScores.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import Download from 'material-ui/svg-icons/file/file-download';
+import Close from 'material-ui/svg-icons/navigation/close';
+import Dropzone from 'react-dropzone';
+import { Link } from 'react-router';
+import { List, ListItem } from 'material-ui/List';
 
 export default class GroupScores extends React.Component {
     static propTypes = {
@@ -9,16 +14,36 @@ export default class GroupScores extends React.Component {
     }
 
     render() {
+        const del = <IconButton><Close /></IconButton>;
         return (
             <div>
                 <h3>{this.props.name}</h3>
-                <div>
+                <Dropzone
+                    style={{
+                        minWidth: 300,
+                        minHeight: 50,
+                        borderWidth: 2,
+                        borderColor: '#666',
+                        borderStyle: 'dashed',
+                        borderRadius: 5,
+                    }}
+                />
+                <List>
                     {this.props.scores.map(
-                        file => <div key={`${this.props.id}-${file.id}`}>
-                            <FlatButton href={file.path} label={file.filename} />
-                        </div>
-                    )}
-                </div>
+                        file => <Link
+                            key={`${this.props.id}-${file.id}`}
+                            href={file.path}
+                            download
+                        >
+                            <ListItem
+                                primaryText={file.filename}
+                                leftIcon={<Download />}
+                                rightIconButton={del}
+                            />
+                        </Link>
+                        )
+                    }
+                </List>
             </div>
         );
     }
