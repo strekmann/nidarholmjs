7b2a03aaf0cc2699a88000028d071f8f62ebb86d
Sigurd Gartmann <sigurdga@sigurdga.no>
2017-02-01 21:35:39 +0100

    Unsaved file, didn't get this in earlier.

--- src/common/components/Projects.js
+++ src/common/components/Projects.js
@@ -161,7 +161,7 @@ export default Relay.createContainer(Projects, {
                         publicMdtext
                         poster {
                             filename
-                            largePath
+                            normalPath
                         }
                     }
                 }
