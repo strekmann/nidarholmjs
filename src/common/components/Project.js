504983fb5d6da3ca1724a46d84fc23d972ec8e00
Sigurd Gartmann <sigurdga@sigurdga.no>
2017-02-13 22:48:07 +0100

    Show thumbnails for project files

--- src/common/components/Project.js
+++ src/common/components/Project.js
@@ -395,7 +395,7 @@ export default Relay.createContainer(Project, {
                             }
                             tags
                             isImage
-                            normalPath
+                            thumbnailPath
                             path
                         }
                     }
