d28f81d6f759355712c9072a73e4845a9be8a0d7
Sigurd Gartmann <sigurdga@sigurdga.no>
2017-03-21 00:31:33 +0100

    Remove linter warnings

--- src/common/components/MusicList.js
+++ src/common/components/MusicList.js
@@ -5,20 +5,20 @@ import MusicItem from './MusicItem';
 export default class MusicList extends React.Component {
     static propTypes = {
         music: React.PropTypes.array,
-        memberGroupId: React.PropTypes.string,
         isMember: React.PropTypes.bool,
     }
     render() {
         return (
             <div>
-                {this.props.music.map(music => (
-                    <MusicItem
-                        key={music.piece.id}
-                        isMember={this.props.isMember}
-                        {...music}
-                    />
-                    ))
-                }
+                {this.props.music.map((music) => {
+                    return (
+                        <MusicItem
+                            key={music.piece.id}
+                            isMember={this.props.isMember}
+                            {...music}
+                        />
+                    );
+                })}
             </div>
         );
     }
