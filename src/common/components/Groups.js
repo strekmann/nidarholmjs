6e48049747dcf8f4d253d7c5042bdc2bcf805840
Sigurd Gartmann <sigurdga@sigurdga.no>
2019-02-05 20:45:06 +0100

    Mobile friendlier group list

--- src/common/components/Groups.js
+++ src/common/components/Groups.js
@@ -2,7 +2,9 @@
 
 import Link from "found/lib/Link";
 import Paper from "material-ui/Paper";
+import { List, ListItem } from "material-ui/List";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
+import GridOff from "material-ui/svg-icons/image/grid-off";
 import PropTypes from "prop-types";
 import * as React from "react";
 import { createFragmentContainer, graphql } from "react-relay";
@@ -41,14 +43,20 @@ class Groups extends React.Component<Props> {
       <section>
         <Paper className="row">
           <h1>Alle grupper</h1>
-          {groups.map((group) => {
-            return (
-              <div key={group.id}>
-                <Link to={`/group/${group.id}`}>{group.name}</Link>
-                {group.externallyHidden ? " h" : null}
-              </div>
-            );
-          })}
+          <List>
+            {groups.map((group) => {
+              return (
+                <ListItem
+                  disabled
+                  key={group.id}
+                  rightIcon={group.externallyHidden ? <GridOff /> : null}
+                  primaryText={
+                    <Link to={`/group/${group.id}`}>{group.name}</Link>
+                  }
+                />
+              );
+            })}
+          </List>
         </Paper>
       </section>
     );
